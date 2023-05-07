bd6257500a3408c8ce4367e163bca0104d8a139a
rahul2409 <rahul_nandrajog@yahoo.com>
2023-05-07 15:34:28 +0530

    Added exception when studentId was greater than the list size

---
+++ src/main/java/com/rest/springbootdemo/demo/RestController/StudentErrorResponse.java
@@ -0,0 +1,37 @@
+package com.rest.springbootdemo.demo.RestController;
+
+public class StudentErrorResponse {
+
+	private int status;
+	private String message;
+	private long timeStamp;
+	
+	public StudentErrorResponse() {
+	}
+	public StudentErrorResponse(int status, String message, long timeStamp) {
+		this.status = status;
+		this.message = message;
+		this.timeStamp = timeStamp;
+	}
+	
+	// Initialize getters and setters
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+}
--- src/main/java/com/rest/springbootdemo/demo/RestController/StudentRestController.java
+++ src/main/java/com/rest/springbootdemo/demo/RestController/StudentRestController.java
@@ -2,13 +2,15 @@ package com.rest.springbootdemo.demo.RestController;
 
 // Basic imports 
 import java.util.*;
-import java.io.*;
 
 // Project specific imports
 import com.rest.springbootdemo.demo.entity.*;
+import com.rest.springbootdemo.demo.RestController.StudentErrorResponse;
 
 import jakarta.annotation.*;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController 
@@ -31,6 +33,10 @@ public class StudentRestController {
 	// Get student by id endpoint is ("/api/student/{studentId}")
 	@GetMapping("/students/{studentId}")
 	public Student getStudentById(@PathVariable int studentId) {
+		// Check the studentId against list size 
+		if(studentId >= theStudents.size() || (studentId<0)) {
+			throw new StudentNotFoundException("Student not found - "+ studentId);
+		}
 		return theStudents.get(studentId);
 	}
 	
@@ -39,4 +45,18 @@ public class StudentRestController {
 	public List<Student> getStudents(){
 		return theStudents;
 	}
+	
+	// Add an exception handler 
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException	(StudentNotFoundException exc){
+		// Create StudentErrorResponse and return the response 
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity(error, HttpStatus.NOT_FOUND);
+	}
+	
 }
